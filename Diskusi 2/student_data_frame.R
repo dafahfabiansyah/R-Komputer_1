1c1252fdf8383ddbc0b80e2037408dcc398cf3eb
dafahfabiansyah <davarobiansyah@gmail.com>
2025-04-30 10:08:40 +0700

    revisi tugas

--- Diskusi 2/student_data_frame.R
+++ Diskusi 2/student_data_frame.R
@@ -1,6 +1,14 @@
-Siswa <- data.frame(
-  Jurusan_asal = rep(c('komputer', 'matematika'), times = 12),
-  Daerah =rep(rep(c("jakarta", "surabaya", "makassar"), each = 2), times = 4),
-  Usia = rep(25:28, each = 6)
-)
-Siswa
+# Siswa <- data.frame(
+#   Jurusan_asal = rep(c('komputer', 'matematika'), times = 12),
+#   Daerah =rep(rep(c("jakarta", "surabaya", "makassar"), each = 2), times = 4),
+#   Usia1 = rep(25:28, each = 6),
+#   Usia2 = rep(c(25:28), each = 6)
+# )
+# Siswa
+
+# revisi
+Jurusan_asal <- rep(c("Komputer", "Matematika"), 12)
+Daerah <- rep(c("Jakarta", "Surabaya", "Makassar"), each = 2, times = 4)
+Usia <- rep(25:28, each = 6)
+data_siswa <- data.frame(Jurusan_asal, Daerah, Usia)
+data_siswa
